dca48c0d2b32a60ba14e486fc6eb1d25004b71fa
Aleksandar Gulevski <agulevski10@gmail.com>
2020-11-27 08:52:04 +0100

    Refactoring

--- app/src/main/java/com/denofdevelopers/mvvm_hilt_coroutines_livedata_room_projectone/data/entities/episode/Episode.kt
+++ app/src/main/java/com/denofdevelopers/mvvm_hilt_coroutines_livedata_room_projectone/data/entities/episode/Episode.kt
@@ -5,14 +5,14 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "episodes")
-data class Episode (
-val name: String,
-@PrimaryKey
-val id: Int,
-@SerializedName("air_date")
-val airDate: String,
-val episode: String,
-val characters: List<String>,
-val url: String,
-val created: String
+data class Episode(
+    val name: String,
+    @PrimaryKey
+    val id: Int,
+    @SerializedName("air_date")
+    val airDate: String,
+    val episode: String,
+    val characters: List<String>,
+    val url: String,
+    val created: String
 )
